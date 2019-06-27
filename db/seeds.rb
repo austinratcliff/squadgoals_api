6ff301a4a15062aa6fc58f717faa070929032201
austinratcliff <hello@austinratcliff.com>
2019-06-27 16:13:21 -0500

    Migrate and seed User DB table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+austin = User.create(
+  first_name: 'Austin',
+  last_name: 'Ratcliff',
+  name: 'Austin Ratcliff',
+  email: 'austin@example.com',
+  phone: '1234567890',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+tyle = User.create(
+  first_name: 'Tyle',
+  last_name: 'Bivens',
+  name: 'Tyle Bivens',
+  email: 'tyle@example.com',
+  phone: '1234567890',
+  password: 'password',
+  password_confirmation: 'password'
+)
